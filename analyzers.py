5061a58a5d2a05cdd787a8fc90f4b3163635fde2
momo <momooooo523@gmail.com>
2024-08-14 14:17:34 +0800

    mod: 增加注释，厘清脚本逻辑

--- analyzers.py
+++ analyzers.py
@@ -117,10 +117,43 @@ class CustomTradeAnalyzer(bt.Analyzer):
             '最长回撤期': max_drawdown_duration,
         }
 
-    
+'''
+类 自定义交易分析器:
+    方法 初始化(交易年数):
+        初始化交易记录、统计数据和资金曲线
+        记录交易年数
+
+    方法 订单通知(订单):
+        如果 订单已完成:
+            更新资金曲线
+
+            如果 是开仓订单:
+                记录当前交易信息(入场价格、数量、价值、日期、入场K线索引)
+
+            否则如果 是加仓订单:
+                更新当前交易信息(平均入场价格、总数量、总价值)
+            
+            否则如果 是平仓订单:
+                计算退出价值、入场价值和利润
+                更新交易统计(总次数、盈亏次数、总盈亏)
+                记录完整交易信息(入场价格,出场价格,盈亏,仓位,出场K线索引)
+                更新剩余仓位
+                如果 完全平仓:
+                    重置当前交易为空
+
+    方法 计算最大回撤():
+        遍历资金曲线，计算最大回撤百分比和最长回撤期
+        返回最大回撤和最长回撤期
+
+    方法 获取分析结果():
+        计算各项交易统计指标
+        返回包含所有分析结果的字典
+            总交易次数、盈亏次数、胜率、亏损率、平均盈利、平均亏损、盈亏比
+            总利润、总亏损、总收益率、年化收益率、最大回撤、最长回撤期
+'''    
 # 计算收益
 class CustomReturns(bt.Analyzer):
-    params = (('timeframe', bt.TimeFrame.Days), ('num_years', 1.0),)  # 添加num_years参数
+    params = (('num_years', 1.0),)  
 
     def start(self):
         self.start_value = self.strategy.broker.getvalue()
@@ -134,13 +167,37 @@ class CustomReturns(bt.Analyzer):
 
     def stop(self):
         self.roi = (self.current_value / self.start_value) - 1.0
-        self.annualized_roi = math.pow(1.0 + self.roi, 1 / self.params.num_years) - 1.0  # 使用实际的交易年数
+        self.annualized_roi = math.pow(1.0 + self.roi, 1 / self.params.num_years) - 1.0
 
     def get_analysis(self):
         return {
             'roi': self.roi,
             'annualized_roi': self.annualized_roi,
         }
+'''
+类 自定义收益分析器:
+    参数 = 使用 num_years
+
+    方法 开始():
+        初始资金 = 获取策略当前总资产
+        当前资产 = 初始资金
+        收益列表 = 空列表
+
+    方法 每次迭代():
+        当前资产 = 获取策略当前总资产
+        收益率 = (当前资产 / 初始资金) - 1.0
+        将收益率添加到收益列表
+
+    方法 结束():
+        总收益率 = (当前资产 / 初始资金) - 1.0
+        年化收益率 = (1.0 + 总收益率) 的 (1 / 交易年数) 次方 - 1.0
+
+    方法 获取分析结果():
+        返回 {
+            '总收益率': 总收益率,
+            '年化收益率': 年化收益率
+        }
+'''
 
 # 最大回撤
 class CustomDrawDown(bt.Analyzer):
@@ -180,3 +237,44 @@ class CustomDrawDown(bt.Analyzer):
                 'len': self.max_drawdown_length
             }
         }
+
+
+'''
+类 自定义回撤分析器:
+    参数 = (('资金', 无),)
+
+    函数 开始():
+        self.当前回撤 = 0.0
+        self.最大回撤 = 0.0
+        self.峰值 = 负无穷大
+        self.回撤开始时间 = 0
+        self.回撤持续时间 = 0
+        self.最大回撤持续时间 = 0
+        self.当前回撤持续时间 = 0
+
+    函数 下一步():
+        当前价值 = self.策略.经纪人.获取价值()
+        
+        如果 当前价值 > self.峰值:
+            self.峰值 = 当前价值
+            self.回撤开始时间 = 数据长度
+            self.当前回撤持续时间 = 0
+        否则:
+            回撤 = (self.峰值 - 当前价值) / self.峰值
+            self.当前回撤 = 回撤
+            self.当前回撤持续时间 += 1
+            
+            如果 回撤 > self.最大回撤:
+                self.最大回撤 = 回撤
+                self.回撤持续时间 = self.当前回撤持续时间
+                self.最大回撤持续时间 = 最大值(self.最大回撤持续时间, self.回撤持续时间)
+
+    函数 获取分析结果():
+        返回 {
+            '最大': {
+                '回撤率': self.最大回撤,
+                '回撤金额': self.峰值 * self.最大回撤,
+                '持续时间': self.最大回撤持续时间
+            }
+        }
+'''
